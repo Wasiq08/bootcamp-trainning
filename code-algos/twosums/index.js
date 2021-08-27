e06fe5177dfef94c0203478cb630e7217df0891e
unknown <s.wasiq.muhammad@gmail.com>
2021-08-27 22:57:56 +0500

    changes

---
+++ code-algos/twosums/index.js
@@ -0,0 +1,18 @@
+function twoSum(arr, S) {
+    let hashTable = {};
+    // check each element in array
+    for (let i = 0; i < arr.length; i++) {
+    // calculate S - current element
+    let sumMinusElement = S - arr[i];
+    // check if this number exists in hash table
+        if (hashTable[sumMinusElement] !== undefined) {
+            return true;
+        }
+    // add the current number to the hash table
+        hashTable[arr[i]] = true;
+    
+    }
+    return false;
+    }
+    
+console.log(twoSum([1,3,4], 5))
---
+++ exercises/ex_3.md
@@ -0,0 +1,10 @@
+Exercise # 3 
+ 1) Create a simple html page which shows one text box if a user write any color code it will change the background color of page
+ 2) Create a simple header and a button. If a user click on the button increase the size of the header
+ 3) Create a simple form with following fields
+	Name (textbox)
+	Gender (radio button)
+	Address (textbox)
+	Submit button 
+	
+	once your submit the form show all the values in a table
